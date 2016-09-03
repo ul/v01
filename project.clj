249b68f84bc1cd4187e7d9e4c8cc1c3e70d57016
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-09-04 00:12:44 +0300

    add project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject v01 "0.1.0-SNAPSHOT"
+  :description "The executable digital music album"
+  :url "https://github.com/violetband/v01"
+  :license {:name "MIT License"
+            :url "https://github.com/violetband/v01/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
+                 [org.clojure/clojurescript "1.9.227"]
+                 [kunstmusik/pink "0.3.0"]
+                 [kunstmusik/score "0.3.0"]]
+  :plugins [[lein-ancient "0.6.10"]])
