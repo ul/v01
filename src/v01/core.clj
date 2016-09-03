96d046980b14a0d3650b34451e126e412fc7bc8d
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-09-04 02:26:59 +0300

    drones ftw

--- src/v01/core.clj
+++ src/v01/core.clj
@@ -1,20 +1,68 @@
 (ns v01.core
   "Where things are assembled and start making noise"
   (:require [pink.simple :as pink]
-            [pink.oscillators :as osc])
+            [pink.oscillators :as osc]
+            [pink.envelopes :as env]
+            [pink.util :as util])
   (:gen-class))
+
+(require '[pink.envelopes :as env])
 
 (pink/start-engine)
 
-(def gen1 (osc/sine 440))
+;;(def gen1 (osc/sine 440))
 
-(pink/add-afunc gen1)
+;;(pink/add-afunc gen1)
 
-(Thread/sleep 5000)
+;;(Thread/sleep 5000)
 
-(pink/remove-afunc gen1)
+;;(pink/remove-afunc gen1)
 
-(pink/stop-engine)
+;;(pink/stop-engine)
+
+(defn e [] (util/with-duration 5.0 (env/adsr 0.05 0.2 0.8 1.0)))
+
+(defn blit-pulse-fucking-awesome [freq pulse-width]
+  (osc/blit-pulse
+    freq
+
+    (util/sum
+      (osc/sine2 pulse-width)
+      (util/mul
+        0.05
+        (osc/sine2 pulse-width (* 0.5 Math/PI)))
+      (util/mul
+        0.05
+        (osc/sine2 pulse-width (* 1.0 Math/PI)))
+      (util/mul
+        0.05
+        (osc/sine2 pulse-width (* 1.5 Math/PI)))
+      )))
+
+(defn gen2 []
+  (let [e (e)]
+    (util/mul e (blit-pulse-fucking-awesome 80 e))))
 
 (defn -main [& args]
-  )
+  (let [bpm 60
+        tact (/ 60000 bpm)
+        n 8
+        step (/ tact n)]
+    (future
+      (dotimes [i 16]
+        (let [delay (/ i 8.0)]
+          (when (> (rand) 0.1)
+            (future
+              (Thread/sleep (* 1 step))
+              (pink/add-afunc (gen2))))
+          (when (> (rand) 0.1)
+            (future
+              (Thread/sleep (* 4 step))
+              (pink/add-afunc (gen2))))
+          (when (> (rand) 0.1)
+            (future
+              (Thread/sleep (* delay step))
+              (pink/add-afunc (gen2))))
+          (Thread/sleep tact))))
+    (Thread/sleep (* 16 tact))
+    (pink/stop-engine)))
